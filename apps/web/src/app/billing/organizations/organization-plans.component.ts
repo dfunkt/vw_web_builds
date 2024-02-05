88d3ba3a5a6e144764c4ccc3201bfc4862c16c81
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-02-05 22:19:01 +0100

    freely allow org creation
    disable license upload for selfhosted and just select the free plan

--- apps/web/src/app/billing/organizations/organization-plans.component.ts
+++ apps/web/src/app/billing/organizations/organization-plans.component.ts
@@ -61,7 +61,6 @@ import {
   EnterPaymentMethodComponent,
   getBillingAddressFromForm,
 } from "@bitwarden/web-vault/app/billing/payment/components";
-import { tokenizablePaymentMethodToLegacyEnum } from "@bitwarden/web-vault/app/billing/payment/types";
 
 import { OrganizationCreateModule } from "../../admin-console/organizations/create/organization-create.module";
 import { PremiumOrgUpgradeService } from "../individual/upgrade/premium-org-upgrade-payment/services/premium-org-upgrade.service";
@@ -173,9 +172,10 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
 
   readonly freeTrial = computed(() => this.selectedPlan()?.trialPeriodDays != null);
 
-  readonly planOffersSecretsManager = computed(() => this.selectedSecretsManagerPlan() != null);
+  readonly planOffersSecretsManager = computed(() => false); // no support for secrets manager in Vaultwarden
 
   readonly selectableProducts = computed(() => {
+    return null; // there are no products to select in Vaultwarden
     if (this.acceptingSponsorship()) {
       const familyPlan = this.passwordManagerPlans.find((plan) => plan.type === this._familyPlan);
       return [familyPlan];
@@ -221,6 +221,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
   });
 
   readonly selectablePlans = computed(() => {
+    return null; // no plans to select in Vaultwarden
     const selectedProductTierType = this.formValues().productTier;
     const result =
       this.passwordManagerPlans?.filter((plan) => {
@@ -240,7 +241,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
   });
 
   readonly teamsStarterPlanIsAvailable = computed(() =>
-    this.selectablePlans().some((plan) => plan.type === PlanType.TeamsStarter),
+    this.selectablePlans()?.some((plan) => plan.type === PlanType.TeamsStarter),
   );
 
   protected readonly showTaxIdField = computed<boolean>(() => {
@@ -500,9 +501,11 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
       await this.loadExistingOrganizationData(organizationId);
     }
 
+    /* no need to ask /api/plans because Vaultwarden only supports the free plan
     if (!this.selfHosted) {
       await this.loadPlanData();
     }
+    */
 
     this._familyPlan = await this.determineFamilyPlan();
 
@@ -514,6 +517,8 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
     if (this.hasProvider()) {
       await this.setupProviderConfiguration();
     }
+
+    /* end of asking /api/plans in Vaultwarden */
 
     if (!this.createOrganization()) {
       this.upgradeFlowPrefillForm();
@@ -573,6 +578,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
   }
 
   get upgradeRequiresPaymentMethod() {
+    return false; // Vaultwarden is always free
     return (
       this.organization?.productTierType === ProductTierType.Free &&
       !this.showFree() &&
@@ -703,6 +709,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
   }
 
   changedProduct() {
+    return; // no choice of products in Vaultwarden
     const selectedPlan = this.selectablePlans()[0];
 
     if (!selectedPlan) {
@@ -806,13 +813,14 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
       return;
     }
 
-    // Validate billing form for paid plans during creation
+    /* don't validate in Vaultwarden because we have no plan selected
     if (this.createOrganization() && this.selectedPlan()?.type !== PlanType.Free) {
       this.billingFormGroup.markAllAsTouched();
       if (this.billingFormGroup.invalid) {
         return;
       }
-    }
+    } */
+
     const doSubmit = async (): Promise<string> => {
       let orgId: string;
       if (this.createOrganization()) {
@@ -915,6 +923,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
   }
 
   private async refreshSalesTax(): Promise<void> {
+    return; // no taxes in Vaultwarden
     if (
       this.billingFormGroup.controls.billingAddress.invalid ||
       this.selectedPlan()?.type === PlanType.Free
@@ -1048,6 +1057,9 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
       encryptionData.orgKeys[1].encryptedString as string,
     );
 
+    request.planType = PlanType.Free; // always select the free plan in Vaultwarden
+
+    /* there is no plan to select in Vaultwarden
     if (this.selectedPlan()!.type === PlanType.Free) {
       request.planType = PlanType.Free;
     } else {
@@ -1083,6 +1095,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
 
     // Secrets Manager
     this.buildSecretsManagerRequest(request);
+    end plan selection and no support for secret manager in Vaultwarden */
 
     if (this.hasProvider()) {
       const providerRequest = new ProviderOrganizationCreateRequest(
@@ -1133,6 +1146,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
   private buildSecretsManagerRequest(
     request: OrganizationCreateRequest | OrganizationUpgradeRequest,
   ): void {
+    return; // Vaultwarden does not support SecretsManager
     const formValues = this.secretsManagerForm.value;
 
     request.useSecretsManager = this.planOffersSecretsManager() && (formValues.enabled ?? false);
@@ -1151,6 +1165,7 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
   }
 
   private upgradeFlowPrefillForm() {
+    return; // Vaultwarden only supports free plan
     if (this.acceptingSponsorship()) {
       this.formGroup.controls.productTier.setValue(ProductTierType.Families);
       this.changedProduct();
@@ -1204,16 +1219,8 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
       (await firstValueFrom(
         this.organizationService.organizations$(userId!).pipe(getOrganizationById(organizationId)),
       )) ?? null;
-    this.billing = await this.organizationApiService.getBilling(organizationId);
-    this.sub = await this.organizationApiService.getSubscription(organizationId);
-    const billingAddress = await this.subscriberBillingClient.getBillingAddress({
-      type: "organization",
-      data: this.organization!,
-    });
-    this.billingFormGroup.controls.billingAddress.patchValue({
-      ...billingAddress,
-      taxId: billingAddress?.taxId?.value,
-    });
+    this.billing = null; // no billing in Vaultwarden
+    this.sub = null; // no subscriptions in Vaultwarden
   }
 
   /**
