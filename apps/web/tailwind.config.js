97b6bb10a3da275bc69893d2a2e97bc4e85ccf57
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-05-21 22:58:40 +0200

    use only gpl code
    remove all references to non-free bitwarden_license code

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,7 +6,6 @@ const config = require("../../libs/components/tailwind.config.base");
 // Add web-specific paths here. Shared libs should go in tailwind.config.base.js instead
 const webContent = [
   path.resolve(__dirname, "./src/**/*.{html,ts,mdx}"),
-  path.resolve(__dirname, "../../bitwarden_license/bit-web/src/**/*.{html,ts,mdx}"),
 ];
 
 config.content = [...config.content, ...webContent];
