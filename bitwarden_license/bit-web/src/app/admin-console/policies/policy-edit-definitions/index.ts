4138a49c9969d16a5e23617d9ab29656172faeba
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 18:41:19 +0100

    remove non-free code from bitwarden_license

--- bitwarden_license/bit-web/src/app/admin-console/policies/policy-edit-definitions/index.ts
+++
@@ -1,3 +0,0 @@
-export { ActivateAutofillPolicy } from "./activate-autofill.component";
-export { AutomaticAppLoginPolicy } from "./automatic-app-login.component";
-export { DisablePersonalVaultExportPolicy } from "./disable-personal-vault-export.component";
