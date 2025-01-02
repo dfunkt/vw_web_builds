b96ef1d86e24e2bc280a8d545aa0edc7e61fc744
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 18:41:19 +0100

    remove non-free code from bitwarden_license

--- bitwarden_license/bit-common/src/dirt/vault-health/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./risk-category";
-export * from "./vault-health-report-state";
-export * from "./vault-health-report-status";
-export * from "./view/vault-health-report.view";
--- bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/models/requests/service-account.request.ts
+++
@@ -1,8 +0,0 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
-// eslint-disable-next-line no-restricted-imports
-import { EncString } from "@bitwarden/legacy-crypto";
-
-export class ServiceAccountRequest {
-  name: EncString;
-}
