90374186d8ad371f7ea0156b58820e581ba86b83
Stefan Melmuk <stefan.melmuk@gmail.com>
2026-07-21 19:14:35 +0200

    allow vw- prefix in class names

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -266,7 +266,7 @@ export default tseslint.config(
         {
           // uses negative lookahead to whitelist any class that doesn't start with "tw-"
           // in other words: classnames that start with tw- must be valid TailwindCSS classes
-          whitelist: ["(?!(tw)\\-).*", "tw-app-region-drag", "tw-app-region-no-drag"],
+          whitelist: ["(?!(tw)\\-).*", "tw-app-region-drag", "tw-app-region-no-drag", "vw-.*"],
         },
       ],
       "tailwindcss/enforces-negative-arbitrary-values": "error",
@@ -438,6 +438,7 @@ export default tseslint.config(
             "filter.*", // Temporary until filters are migrated
             "tw-app-region*", // Custom utility for native passkey modals
             "tw-@container",
+            "vw-.*",
           ],
         },
       ],
